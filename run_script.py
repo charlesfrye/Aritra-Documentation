42c494fa113cd93146550a27fd59f59bc8c2c382
Aritra Roy Gosthipaty <aritra.born2fly@gmail.com>
2020-11-12 13:25:54 +0530

    markdown files

--- run_script.py
+++ run_script.py
@@ -7,4 +7,5 @@ for dir in list_dir:
         os.mkdir('DOCS/{}'.format(dir))
     for f in list_file:
         # print("python doc_generator.py CODE/{0}/{1} DOCS/{0}/{1}".format(dir,f))
-        os.system("python doc_generator.py CODE/{0}/{1} DOCS/{0}/{1}".format(dir,f))
+        f_n = f.split('.')[0]
+        os.system("python doc_generator.py CODE/{0}/{1}.py DOCS/{0}/{1}.md".format(dir,f_n))
